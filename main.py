2d4ff4ba0ae3438e6cfac0347d71133423957e2a
Leela Manohar Gudivada <mgudivada@msa.miraclesoft.com>
2025-07-16 16:33:51 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import schedule
+import time
+import requests
+from datetime import datetime
+import pytz
+from dotenv import dotenv_values
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+config = dotenv_values(".env")
+
+CHANNEL_WEBHOOK_URL=config['CHANNEL_WEBHOOK_URL']
+BOT_WEBHOOK_URL=config['BOT_WEBHOOK_URL']
+
+
+def send_reminder():
+    payload = {
+        "text": "🔔 *Reminder*: Submit your timesheets today by EOD!"
+    }
+    try:
+        # print("Entered")
+        response = requests.post(CHANNEL_WEBHOOK_URL, json=payload, verify=False)
+        print("Request sent")
+        response.raise_for_status()
+        print("Reminder sent successfully.")
+    except requests.exceptions.RequestException as e:
+        print("Error sending reminder:", e)
+
+def schedule_reminder():
+    ist = pytz.timezone('Asia/Kolkata')
+
+    def reminder():
+        current_time = datetime.now(ist)
+        print(current_time)
+        if current_time.weekday() == 2 and current_time.hour == 16 and current_time.minute == 17:
+            send_reminder()
+
+    schedule.every().minute.do(reminder)
+
+if __name__ == "__main__":
+    print("Reminder bot started")
+    schedule_reminder()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
