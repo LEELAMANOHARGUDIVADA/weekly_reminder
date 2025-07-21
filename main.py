259a6a767f1431fee18d83343e45f56683cb98f7
Leela Manohar Gudivada <mgudivada@msa.miraclesoft.com>
2025-07-21 20:02:35 +0530

    added alert_history feature

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import threading
 from email.mime.text import MIMEText
 import schedule
 import time
@@ -13,6 +15,8 @@ import pandas as pd
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 config = dotenv_values()
 
+messages = json.load(open("data/alert_messages.json"))
+emailData = json.load(open("data/alert_emails.json"))
 app = Flask(__name__)
 s = smtplib.SMTP(config['SMTP_ZOHO_SERVER'], int(config['SMTP_PORT']))
 s.starttls()
@@ -21,15 +25,16 @@ s.login(config['SENDER_ZOHO_EMAIL'], config['SENDER_ZOHO_PASSWORD'])
 BOT_WEBHOOK_URL=config['BOT_WEBHOOK_URL']
 CHANNEL_WEBHOOK_URL=config['CHANNEL_WEBHOOK_URL']
 
-def send_reminder():
+def send_reminder(current_time, message):
     payload = {
-        "text": "🔔 *Reminder*: Submit your timesheets today by EOD!"
+        "text": message
     }
     try:
         # print("Entered")
-        response = requests.post(CHANNEL_WEBHOOK_URL, json=payload, verify=False)
+        response = requests.post(BOT_WEBHOOK_URL, json=payload, verify=False)
         print("Request sent")
         response.raise_for_status()
+        update_alert_history(current_time=current_time, message=message)
         print("Reminder sent successfully")
     except requests.exceptions.RequestException as e:
         print("Error sending reminder:", e)
@@ -40,12 +45,21 @@ def schedule_reminder():
     def reminder():
         current_time = datetime.now(ist)
         print(current_time)
-        if current_time.weekday() == 4 and current_time.hour == 19 and current_time.minute == 0:
-            send_reminder()
+        if current_time.weekday() == 0 and current_time.hour == 19 and current_time.minute == 46:
+            send_reminder(current_time, messages['messages'][0])
 
     schedule.every().minute.do(reminder)
 
-
+def update_alert_history(current_time, message):
+    data = json.load(open('data/alert_history.json'))
+    data['history'].append({
+        "id": data['history'][-1]['id'] + 1,
+        "message": message,
+        "alert_date": str(current_time)
+    })
+    with open('data/alert_history.json', 'w') as file:
+        json.dump(data, file)
+        print("Alert History Updated")
 @app.route('/send-emails', methods=['POST'])
 def send_emails():
     if "file" not in request.files:
@@ -58,14 +72,8 @@ def send_emails():
     try:
 
         for email in emails:
-            message = MIMEText("""
-                Hi,
-
-                You haven't submitted your timesheet this week. Please submit it as soon as possible.
-
-                Thank you
-            """)
-            message['Subject'] = "Timesheet Not Submitted"
+            message = MIMEText(emailData['emails'][0]['message'])
+            message['Subject'] = emailData['emails'][0]['subject']
             message['From'] = config['SENDER_ZOHO_EMAIL']
             message['To'] = email
             print(message['To'])
@@ -75,11 +83,17 @@ def send_emails():
     except smtplib.SMTPException as e:
         return jsonify(success=False, message=str(e))
 
+def run_flask():
+    print("Flask App Running")
+    app.run(debug=True, use_reloader=False)
 
 if __name__ == "__main__":
     print("Reminder bot started")
     schedule_reminder()
-    app.run(debug=True)
+    thread = threading.Thread(target=run_flask)
+
+    thread.daemon = True
+    thread.start()
 
     while True:
         schedule.run_pending()
